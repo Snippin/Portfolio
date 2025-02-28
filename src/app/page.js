9d19d002ffa8498047865fa091465e209c1d2f98
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-01 01:07:44 +0800

    Update `Home Page` to have navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
+import Navbar from "../components/Navbar";
+
 export default function Home()
 {
   return (
-    <>
-      <div className="font-normal">
-        temp
-      </div>
-    </>
+    <Navbar currentPage="Home"></Navbar>
   );
 }
