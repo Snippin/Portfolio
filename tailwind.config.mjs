ca10e0aaec62c9b83c4db17bc043729dd0e4e21b
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-01 04:35:05 +0800

    Add in a color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,8 +12,22 @@ export default {
     {
       colors:
       {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        darkA0: "var(--dark-a0)",
+        lightA0: "var(--light-a0)",
+
+        primaryA0: "var(--primary-a0)",
+        primaryA10: "var(--primary-a10))",
+        primaryA20: "var(--primary-a20)",
+        primaryA30: "var(--primary-a30)",
+        primaryA40: "var(--primary-a40)",
+        primaryA50: "var(--primary-a50)",
+
+        surfaceA0: "var(--surface-a0)",
+        surfaceA10: "var(--surface-a10)",
+        surfaceA20: "var(--surface-a20)",
+        surfaceA30: "var(--surface-a30)",
+        surfaceA40: "var(--surface-a40)",
+        surfaceA50: "var(--surface-a50)",
       },
       fontFamily:
       {
