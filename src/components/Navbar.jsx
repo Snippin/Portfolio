552f0d3580e9070423bbc1739718e818dd0046a8
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-03 00:36:50 +0800

    Underline `Navbar` link if user is on the current page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
 
 import Logo from "../../public/images/globe.svg"
+import MainPages from "../app/constants/main_pages";
 
 const Navbar = ({ currentPage = "Home" }) =>
 {
@@ -17,8 +18,13 @@ const Navbar = ({ currentPage = "Home" }) =>
     SetMenuState(!isMenuOpen);
   }
 
+  const CheckCurrentPage = (page) =>
+    currentPage === page ?
+      "ml-10 border-b-2 border-primaryA20 text-primaryA20 text-xl" :
+      "ml-10 hover:border-b text-xl";
+
   return (
-    <nav className="fixed w-full h-24 shadow-xl">
+    <nav className="fixed w-full h-24 shadow-xl bg-surfaceA10">
       <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
         <div className="flex items-center">
           <Link href="/" >
@@ -31,23 +37,23 @@ const Navbar = ({ currentPage = "Home" }) =>
               priority
             />
           </Link>
-          <p className="ml-5 text-xl cursor-default">{currentPage}</p>
+          <p className="ml-5 text-xl cursor-default text-primaryA50">{currentPage}</p>
         </div>
 
 
         <div className="hidden sm:flex">
           <ul className="hidden sm:flex">
             <Link href="/about">
-              <p className="ml-10 hover:border-b text-xl">About</p>
+              <p className={CheckCurrentPage(MainPages.ABOUT)}>About</p>
             </Link>
             <Link href="/contact">
-              <p className="ml-10 hover:border-b text-xl">Contact</p>
+              <p className={CheckCurrentPage(MainPages.CONTACT)}>Contact</p>
             </Link>
             <Link href="/projects">
-              <p className="ml-10 hover:border-b text-xl">Projects</p>
+              <p className={CheckCurrentPage(MainPages.PROJECTS)}>Projects</p>
             </Link>
             <Link href="/mini-projects">
-              <p className="ml-10 hover:border-b text-xl">Mini-Projects</p>
+              <p className={CheckCurrentPage(MainPages.MINI_PROJECTS)}>Mini-Projects</p>
             </Link>
           </ul>
         </div>
@@ -59,9 +65,9 @@ const Navbar = ({ currentPage = "Home" }) =>
       </div>
 
       <div className={
-        isMenuOpen
-          ? "fixed left-0 top-0 sm:hidden w-[65%] h-screen bg-slate-400 p-10 ease-in duration-500"
-          : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+        isMenuOpen ?
+          "fixed left-0 top-0 sm:hidden w-[65%] h-screen bg-surfaceA10 p-10 ease-in duration-500" :
+          "fixed left-[-100%] top-0 p-10 ease-in duration-500"
       }>
         <div className="flex w-full items-center justify-end">
           <div onClick={HandleNav}
