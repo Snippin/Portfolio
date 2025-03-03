1d81f1c89513508ab3c77f07a8de75dc197b8cb7
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-04 04:41:32 +0800

    Update `Footer` to be responsive on smaller screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,28 @@ import Link from "next/link";
 const Footer = () =>
 {
   return (
-    <footer className="flex border-t mx-auto my-5 pt-5">
-      <div className="w-1/6 pl-4">
-        <Link href="https://github.com/Snippin" target="_blank"
-          className="cursor-pointer border-b">
-          GITHUB
-        </Link>
-      </div>
-
-      <div className="w-2/3 px-4 justify-items-center">
+    <footer className="border-t mx-auto my-5 pt-5 sm:flex">
+      <div className=" px-4 justify-items-center sm:w-5/6">
         <p>Contact Information</p>
         <p>seantongch@gmail.com</p>
       </div>
 
-      <div className="w-1/6 pr-4">
-        <Link href="http://linkedin.com/in/sean-tong-ch/" target="_blank"
-          className="cursor-pointer border-b">
-          LINKEDIN
-        </Link>
+      <div className="px-4 mt-4 justify-items-center
+        sm:w-1/6 sm:px-0 sm:pr-4 sm:justify-items-end sm:mt-0">
+        <div>
+          <Link href="http://linkedin.com/in/sean-tong-ch/" target="_blank"
+            className="cursor-pointer border-b">
+            LINKEDIN
+          </Link>
+        </div>
+
+        <div>
+          <Link href="https://github.com/Snippin" target="_blank"
+            className="cursor-pointer border-b">
+            GITHUB
+          </Link>
+        </div>
+
       </div>
     </footer >
   );
