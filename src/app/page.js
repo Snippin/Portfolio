edf7f8217b439615cdd492ad6020e18fb4785760
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-04 03:30:57 +0800

    Add a small description about myself in home page

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,24 @@ export default function Home()
   return (
     <>
       <Navbar currentPage="Home" />
-      <div className="pt-24">
+
+      <div className="pt-28 px-3 md:px-16">
+        <h1>
+          Programmer / Game Developer
+        </h1>
+
+        <p className="text-2xl font-bold italic my-4 md:text-8xl">
+          Sean Tong
+        </p>
+
+        <p>
+          Aspiring programmer, that loves learning about programming,{" "}
+          <span className="inline md:block">
+            as well as having a passion for creating games.
+          </span>
+        </p>
       </div>
+
       <Footer />
     </>
   );
