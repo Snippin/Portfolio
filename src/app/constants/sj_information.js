919ae924f7afe523d122ce8bdc103b93f96f7f01
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-07 01:01:25 +0800

    Create `sj_information` to store information on `Slayer's Journey`

---
+++ src/app/constants/sj_information.js
@@ -0,0 +1,23 @@
+const SJInfo = {
+  name: "Slayer's Journey",
+  roles: "Independent",
+  shortDescription: "Slayer's Journey is a deck-building rogue-like game where players can experiment.",
+  fullDescription: "Slayer's Journey is a game about building a deck that suits the player's playstyle while also experimenting with different cards, continuing to try different synergies, and discovering their preferred way of playing the game.\n\n2D Unity Project",
+  genre: "Rogue-like, Deck Building, RPG",
+  platform: "PC",
+  duration: "2024",
+  images: [
+    "/images/projects/slayers_journey/sj_MainMenu.jpg",
+    "/images/projects/slayers_journey/sj_Combat.jpg",
+    "/images/projects/slayers_journey/sj_Combat2.jpg",
+    "/images/projects/slayers_journey/sj_DeckView.jpg",
+    "/images/projects/slayers_journey/sj_DeckView2.jpg",
+    "/images/projects/slayers_journey/SJ_CombatEnd.jpg",
+    "/images/projects/slayers_journey/SJ_RandomMap.jpg",
+    "/images/projects/slayers_journey/SJ_RandomMap2.jpg",
+    "/images/projects/slayers_journey/SJ_RestSite.jpg",
+    "/images/projects/slayers_journey/SJ_VFX.jpg",
+  ],
+};
+
+export default SJInfo;
