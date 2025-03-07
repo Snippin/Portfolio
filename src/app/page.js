88e8592db8bd7da710caf9e1f4b81f24630b7944
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-08 05:16:36 +0800

    Display 3 works in `Home` page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import BestShowcase from "../components/BestWorkShowcase"
+
+import SJInfo from "./constants/sj_information";
+import BHInfo from "./constants/bh_information";
+import LumenousInfo from "./constants/lumenous_information";
 
 export default function Home()
 {
@@ -7,12 +12,13 @@ export default function Home()
     <>
       <Navbar currentPage="Home" />
 
+      {/* Small introduction */}
       <div className="pt-28 px-3 md:px-16">
         <h1>
           Programmer / Game Developer
         </h1>
 
-        <p className="text-2xl font-bold italic my-4 md:text-8xl">
+        <p className="text-5xl font-bold italic my-4 md:text-9xl">
           Sean Tong
         </p>
 
@@ -22,6 +28,27 @@ export default function Home()
             as well as having a passion for creating games.
           </span>
         </p>
+      </div>
+
+      {/* Best 3 works */}
+      <div className="mt-8">
+        <h1 className="px-3 my-8 text-3xl font-bold italic md:text-5xl md:px-16">
+          BEST WORKS
+        </h1>
+
+        <h1 className="border-t mb-8" />
+
+        <BestShowcase
+          showcaseNum="1"
+          info={SJInfo} />
+
+        <BestShowcase
+          showcaseNum="2"
+          info={LumenousInfo} />
+
+        <BestShowcase
+          showcaseNum="3"
+          info={BHInfo} />
       </div>
 
       <Footer />
