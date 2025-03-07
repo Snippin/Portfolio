dfe4849862d63487f8306a8218897dca36a5c172
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-08 05:12:50 +0800

    Add in `Lumenous` project information

---
+++ src/app/constants/lumenous_information.js
@@ -0,0 +1,22 @@
+const LumenousInfo = {
+  name: "Lumenous",
+  roles: "AI Programmer, Game Programmer, System Coordinator",
+  shortDescription: "Lumenous is a labyrinth puzzle horror game with darkness being your enemy.",
+  fullDescription: "Lumenous is a labyrinth puzzle horror game with darkness being your enemy. Players are to solve the puzzles while avoiding the enemies and try to escape after collecting 4 items scattered in different areas that have different enemy/mechanics to overcome.\n\n3D Unity Project",
+  genre: "First Person, Horror, Survival, Exploration, Puzzle",
+  platform: "PC",
+  duration: "2023",
+  images: [
+    "/images/projects/Lumenous/Lumenous_MainMenu.jpg",
+    "/images/projects/Lumenous/Lumenous_Gameplay1.jpg",
+    "/images/projects/Lumenous/Lumenous_Gameplay2.jpg",
+    "/images/projects/Lumenous/Lumenous_AI1.gif",
+    "/images/projects/Lumenous/Lumenous_AI2.gif",
+    "/images/projects/Lumenous/Lumenous_AI3.1.gif",
+    "/images/projects/Lumenous/Lumenous_AI3.2.gif",
+    "/images/projects/Lumenous/Lumenous_Fog.gif",
+  ],
+  link: "/projects/Lumenous",
+};
+
+export default LumenousInfo;
