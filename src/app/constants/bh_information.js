acaa4b3147502a94522e684743aa49c74b221aed
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-08 04:37:39 +0800

    Create `bh_information` to store information on `Bullet.Hell`

---
+++ src/app/constants/bh_information.js
@@ -0,0 +1,20 @@
+const BHInfo = {
+  name: "Bullet.Hell",
+  roles: "AI Programmer, Game Programmer, System Coordinator",
+  shortDescription: "Up against hordes of enemies, Bullet.Hell puts the player right into the middle of the chaos with the limited arsenal they prepared. Prepare, Struggle and Climb against the mayhem or fall back to zero.",
+  fullDescription: "Bullet.Hell is a rogue-like deck builder where players are to modify their decks which contains types of guns the player may use during the stage. Player has to kill all enemies in the stage to continue.\n\n2D Unity Project",
+  genre: "Bullet Hell, Rogue-like, Deck Building",
+  platform: "PC",
+  duration: "2023",
+  images: [
+    "/images/projects/bullet.hell/BH_MainMenu.jpg",
+    "/images/projects/bullet.hell/BH_Gameplay.jpg",
+    "/images/projects/bullet.hell/BH_Gameplay2.jpg",
+    "/images/projects/bullet.hell/BH_Shop.jpg",
+    "/images/projects/bullet.hell/BH_LevelUp.jpg",
+    "/images/projects/bullet.hell/BH_AI.jpg",
+  ],
+  link: "/projects/Bullet.Hell",
+};
+
+export default BHInfo;
