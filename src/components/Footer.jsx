c6f70f483ab8ac933c063b3017992910ffb4222e
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-07 23:42:28 +0800

    Update `Footer` component's contact information to be at the left

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,16 @@ import { AiOutlineArrowUp } from "react-icons/ai";
 const Footer = () =>
 {
   return (
-    <footer className="border-t mx-auto my-5 pt-5 sm:flex sm:flex-wrap">
-      <div className=" px-4 justify-items-center sm:w-5/6">
+    <footer className="border-t mx-auto my-5 pt-5
+    sm:flex sm:flex-wrap">
+      <div className=" px-4 justify-items-center
+      sm:justify-items-start sm:w-1/2">
         <p>Contact Information</p>
         <p>seantongch@gmail.com</p>
       </div>
 
       <div className="px-4 mt-4 justify-items-center
-        sm:w-1/6 sm:px-0 sm:pr-4 sm:justify-items-end sm:mt-0">
+        sm:px-0 sm:pr-4 sm:mt-0 sm:justify-items-end sm:w-1/2">
         <div>
           <Link href="http://linkedin.com/in/sean-tong-ch/" target="_blank"
             className="cursor-pointer border-b">
@@ -29,12 +31,14 @@ const Footer = () =>
         </div>
       </div>
 
-      <div className="flex flex-col-reverse w-full mt-4 px-4 text-center sm:text-left sm:flex-row sm:justify-between">
+      <div className="flex flex-col-reverse w-full mt-4 px-4 text-center
+      sm:text-left sm:flex-row sm:justify-between">
         <p>
           &copy; {new Date().getFullYear()} by Sean Tong
         </p>
 
-        <div className="flex items-center justify-center mb-4 cursor-pointer sm:mb-0"
+        <div className="flex items-center justify-center mb-4 cursor-pointer
+        sm:mb-0"
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
           <AiOutlineArrowUp size={24} />
