40c5f9e19c5063ab4d7600a36b1534a2dd87ffa1
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-10 04:50:37 +0800

    Set up for deploying to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isProduction ? "/Portfolio" : "",
+  env: {
+    BASE_PATH: isProduction ? "/Portfolio" : "",
+  },
+};
 
 export default nextConfig;
