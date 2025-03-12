ae0d89a2cb114140e8c207043407dce103fe2285
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-12 22:57:02 +0800

    Add gradient hover effect on images

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -6,14 +6,20 @@ import Link from "next/link";
 const ProjectPreview = ({ info }) =>
 {
   return (
-    <Link href={info.link} className="flex flex-col">
-      <Image
-        src={info.image}
-        alt={info.name}
-        width={500}
-        height={500}
-        className="w-full h-auto object-cover rounded-3xl"
-      />
+    <Link href={info.link} className="flex flex-col group relative">
+      <div className="w-full overflow-hidden rounded-3xl relative">
+        <Image
+          src={info.image}
+          alt={info.name}
+          width={500}
+          height={500}
+          className="w-full h-auto object-cover"
+        />
+
+        <div className="absolute inset-0
+          bg-gradient-to-b from-transparent to-primaryA10 opacity-0
+          group-hover:opacity-50 transition-opacity duration-300" />
+      </div>
 
       <p className="font-bold my-4 text-xl
         sm:text-3xl">
