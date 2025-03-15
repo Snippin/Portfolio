1da9e807f0110f3704335fb94179a46da8890afe
Snippin <123628396+Snippin@users.noreply.github.com>
2025-03-16 04:00:24 +0800

    Create `Lumenous` project page

--- src/app/projects/lumenous/lumenous_information.js
+++ src/app/projects/lumenous/lumenous_information.js
@@ -1,4 +1,4 @@
-const basePath = process.env.BASE_PATH;
+const basePath = `${process.env.BASE_PATH}/images/projects/lumenous`;
 
 const LumenousInfo = {
   name: "Lumenous",
@@ -9,19 +9,43 @@ const LumenousInfo = {
   platform: "PC",
   duration: "2023 (2 Weeks)",
   slideshow: [
-    `${basePath}/images/projects/lumenous/Lumenous_MainMenu.jpg`,
-    `${basePath}/images/projects/lumenous/Lumenous_Gameplay1.jpg`,
-    `${basePath}/images/projects/lumenous/Lumenous_Gameplay2.jpg`,
+    `${basePath}/Lumenous_MainMenu.jpg`,
+    `${basePath}/Lumenous_Gameplay1.jpg`,
+    `${basePath}/Lumenous_Gameplay2.jpg`,
   ],
-  images: [
-    `${basePath}/images/projects/lumenous/Lumenous_MainMenu.jpg`,
-    `${basePath}/images/projects/lumenous/Lumenous_Gameplay1.jpg`,
-    `${basePath}/images/projects/lumenous/Lumenous_Gameplay2.jpg`,
-    `${basePath}/images/projects/lumenous/Lumenous_AI1.gif`,
-    `${basePath}/images/projects/lumenous/Lumenous_AI2.gif`,
-    `${basePath}/images/projects/lumenous/Lumenous_AI3.1.gif`,
-    `${basePath}/images/projects/lumenous/Lumenous_AI3.2.gif`,
-    `${basePath}/images/projects/lumenous/Lumenous_Fog.gif`,
+  workGallery: [
+    {
+      image: `${basePath}/Lumenous_AI1.gif`,
+      title: "Dark Stalker AI",
+      description: "When player shines light on him, he gets stunned temporarily",
+    },
+    {
+      image: `${basePath}/Lumenous_AI2.gif`,
+      title: "Shambler AI",
+      description: "Temporarily disables the player's lamp at random intervals\nCan be intercepted with using the flash module when it is in range",
+    },
+    {
+      image: `${basePath}/Lumenous_AI3.1.gif`,
+      title: "Charger AI\n(Roaming animation)",
+      description: "Charges at the player when close enough and in light (in sight)\nStuns itself when it hits a wall and opens up a breakable wall if it hits it",
+    },
+    {
+      image: `${basePath}/Lumenous_AI3.2.gif`,
+      title: "Charger AI\n(Charging animation)",
+      description: "Charges at the player when close enough and in light (in sight)\nStuns itself when it hits a wall and opens up a breakable wall if it hits it",
+    },
+    {
+      image: `${basePath}/Lumenous_Fog.gif`,
+      title: "Fog",
+      description: "Particle system with physics collision\nIncreases the fear meter quicker when inside it\nCan be removed temporarily when using UV module",
+    },
+  ],
+  workList: [
+    "Create finite-state machines to handle AI behaviors",
+    "Learn how to use Unity's `Navmesh` for AI pathfinding",
+    "AI model animations",
+    "Saving & Loading of data (Player data, and AI locations)",
+    "Github code integration (Setting up repository, resolving merge conflicts)",
   ],
   link: "/projects/lumenous",
 };
---
+++ src/app/projects/lumenous/page.js
@@ -0,0 +1,85 @@
+import Image from "next/image";
+
+import Navbar from "../../../components/Navbar"
+import Footer from "../../../components/Footer"
+import ProjectIntroduction from "../../../components/ProjectIntroduction";
+import WorkImageGallery from "../../../components/WorkImageGallery";
+
+import LumenousInfo from "./lumenous_information";
+
+export default function BulletHellPage()
+{
+  return (
+    <>
+      <Navbar currentPage="Bullet.Hell" />
+
+      <div className="pt-28" />
+
+      <ProjectIntroduction
+        roles={LumenousInfo.roles}
+        description={LumenousInfo.fullDescription}
+        duration={LumenousInfo.duration}
+        genre={LumenousInfo.genre}
+        platform={LumenousInfo.platform} />
+
+      <h1 className="border-b-2 border-surfaceA30" />
+
+      {/* Project gameplay showcase */}
+      <div className="grid grid-cols-1 my-8 px-4
+        sm:px-16">
+        {/* Trailer */}
+        <div className="relative w-full pb-[56.25%] h-auto mb-4">
+          <iframe
+            className="absolute top-0 left-0 w-full h-full"
+            src="https://www.youtube.com/embed/Fj3WpWKFDXU?si=so0S7HU3yElQ43Ql"
+            title="Lumenous Trailer"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          />
+        </div>
+
+        {/* Gameplay images */}
+        <div className="flex flex-col gap-8
+          sm:flex-row">
+          {
+            LumenousInfo.slideshow.slice(1).map((image, index) => (
+              <div key={index} className="w-full overflow-hidden rounded-3xl sm:w-1/2">
+                <Image
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  width={500}
+                  height={500}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+      <h1 className="border-y-2 border-surfaceA30 my-8">
+        <p className="font-bold my-8 text-2xl px-5
+          sm:text-5xl sm:px-16">
+          WORK DONE
+        </p>
+      </h1>
+
+      <WorkImageGallery items={LumenousInfo.workGallery} />
+
+      <ul className="px-4 ml-4 mt-4
+        sm:px-16">
+        {
+          LumenousInfo.workList.map((work, index) => (
+            <li
+              key={index}
+              className="list-disc">
+              {work}
+            </li>
+          ))
+        }
+      </ul>
+
+      <Footer />
+    </>
+  );
+}
